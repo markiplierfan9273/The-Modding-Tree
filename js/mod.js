2ea5275d1270b4d1f72bdfa08d08a1c747abd01f
markiplierfan9273 <lucid1892@gmail.com>
2025-04-07 13:29:20 -0700

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "Magic",
+	id: "manamagic",
+	author: "markiplierfan9273",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
